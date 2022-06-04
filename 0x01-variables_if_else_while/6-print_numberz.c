f02fa34d6006b1aa35f069b88597c1645ad65cac
AloisBlue <aloismburu@gmail.com>
2022-06-04 05:53:33 -0700

    putchar digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+*/
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar('0' + n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
