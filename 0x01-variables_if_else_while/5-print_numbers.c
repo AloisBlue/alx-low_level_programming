d380fb1bb0ee9edc23029e220636f96bc98e2efa
AloisBlue <aloismburu@gmail.com>
2022-06-04 05:30:24 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+*/
+int main(void)
+{
+	int dgt;
+
+	for (dgt = 0; dgt < 10; dgt++)
+	{
+		printf("%d", dgt);
+	}
+	printf("\n");
+	return (0);
+}
