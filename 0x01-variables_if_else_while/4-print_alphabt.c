3b7840ab50f450d1434d2b45fd6e4713fadf6cb4
AloisBlue <aloismburu@gmail.com>
2022-06-04 05:24:01 -0700

    no q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+*/
+int main(void)
+{
+	char chr;
+
+	for (chr = 'a'; chr <= 'z'; chr++)
+	{
+		if (!(chr == 'q' || chr == 'e'))
+		{
+			putchar(chr);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
