740d4908c8e825f2c200de539f50720e6dcf962a
AloisBlue <aloismburu@gmail.com>
2022-06-09 03:58:25 -0700

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,16 @@
 #include "hoberton.h"
-#include <stdio.h>
 
 /**
  * _isupper - check for uppercase character
  *
- * @c: this is a character argument
+ * @c: int argument
  *
- * Return: return 0 always
+ * Return: return 0 or 1 based on failure
 */
-int _isupper(char c)
+int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	{
+	if (c >= 65 && c <= 90)
 		return (1);
-	}
-	return (0);
+	else
+		return (0);
 }
