be848a5eab9db67ba076c5e06f9428802919aefc
AloisBlue <aloismburu@gmail.com>
2022-06-12 12:56:22 -0700

    print lowercase * 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print lowercases 10 times
+ *
+ * Return: always 0 for success
+*/
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
