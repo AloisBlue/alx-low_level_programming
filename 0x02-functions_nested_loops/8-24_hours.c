8b5d61eafc546973d359f3550260d0ab78a1e94d
AloisBlue <aloismburu@gmail.com>
2022-07-01 11:35:42 -0700

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,31 +13,43 @@ void jack_bauer(void)
 		for (j = 0; j < 60; j++)
 		{
 			if (i < 10)
+			{
 				if (i < 10)
+				{
 					_putchar('0');
 					_putchar(i);
 					_putchar(':');
 					_putchar('0');
 					_putchar(j);
 					_putchar('\n');
+				}
 				else
+				{
 					_putchar('0');
 					_putchar(i);
 					_putchar(':');
 					_putchar(j);
 					_putchar('\n');
+				}
+			}
 			else
+			{
 				if (j < 10)
+				{
 					_putchar(i);
 					_putchar(':');
 					_putchar('0');
 					_putchar(j);
 					_putchar('\n');
+				}
 				else
+				{
 					_putchar(i);
 					_putchar(':');
 					_putchar(j);
 					_putchar('\n');
+				}
+			}
 		}
 	}
 }
