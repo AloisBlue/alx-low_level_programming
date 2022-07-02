3368447a1cc64ce760310be43ab4ca7b99a4ea24
AloisBlue <aloismburu@gmail.com>
2022-07-02 01:00:58 -0700

    fix more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,18 +6,17 @@
 void more_numbers(void)
 {
 	int i, n;
-	
+
 	i = 0;
 	while (i <= 10)
-	{	
+	{
 		n = 0;
 		while (n <= 14)
 		{
-			_putchar('0' + n);
+			_putchar(n % 10 + '0');
 			n++;
 		}
 		_putchar('\n');
 		i++;
 	}
-	_putchar('\n');
 }
