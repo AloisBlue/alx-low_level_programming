19ad5ae38d8a033cc9d3b36f3f1487c4643f1778
AloisBlue <aloismburu@gmail.com>
2022-07-02 00:52:23 -0700

    fix most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,13 +7,13 @@
  */
 void print_most_numbers(void)
 {
-	char n;
+	int n;
 
-	n = '0';
-	while (n <= '9')
+	n = 0;
+	while (n <= 9)
 	{
-		if (!(n == 2 || n == 4))
-			_putchar(n);
+		if (n != 2 && n != 4)
+			_putchar('0' + n);
 		n++;
 	}
 	_putchar('\n');
