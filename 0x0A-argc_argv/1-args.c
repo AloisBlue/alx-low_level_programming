ae3d134d966c1ba1581d3fb28ded77fa59708caa
AloisBlue <aloismburu@gmail.com>
2022-07-03 21:57:17 -0700

    count args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * @argc: count to the args
+ *
+ * @argv: pointer to string of args
+ *
+ * Return: returns (0) always success
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
