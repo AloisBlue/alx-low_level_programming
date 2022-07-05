5720f9cb185abb405783ae8a7fa61f2a46cfc566
AloisBlue <aloismburu@gmail.com>
2022-07-05 01:30:08 -0700

    allocate array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,6 @@ int **alloc_grid(int width, int height)
 
 	if (arr == 0)
 	{
-		free(arr);
 		return (NULL);
 	}
 
@@ -33,6 +32,7 @@ int **alloc_grid(int width, int height)
 	}
 
 	if (arr == NULL)
+		free(arr);
 		return (NULL);
 
 	return (arr);
