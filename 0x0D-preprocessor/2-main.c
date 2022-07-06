b86019aa1bc3f90376fb9f96e4d66dc8af0dd88c
AloisBlue <aloismburu@gmail.com>
2022-07-06 00:47:42 -0700

    print file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+#include "1-pi.h"
+
+/**
+ * main - entry point
+ *
+ * Return: always returns (0) success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
